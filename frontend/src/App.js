63babffae1ace6810b2458535d4e9a18d91a9c62
Aniruddha <aniruddha.murali@gmail.com>
2021-12-18 14:39:40 -0500

    Create navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,23 +8,50 @@ import {
   Link,
   NavLink
 } from "react-router-dom";
+import {Navbar, Nav, Container, NavDropdown} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/js/bootstrap.js';
+import styled from "styled-components";
 
 import Home from './components/Home';
+
+const Styles = styled.div`
+  .navbar-brand,
+  .navbar-nav .nav-item .nav-link {
+    color: #fff;
+    &:hover {
+      color: #fb7840;
+    }
+  }
+
+  .navbar-toggle-icon {
+    background-image: url("data:image/svg+xml;");
+    color: #fff;
+  }
+`;
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-          </ul>
-        </nav>
+        <Styles>
+          <Navbar bg="dark" expand="lg">
+            <Container>
+              <Navbar.Brand href="/">Aniruddha Murali</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Item>
+                    <Nav.Link href="/">Home</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link href="/about">About</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </Styles>
 
         <Routes>
           <Route path="/" element={<Home/>} />
