3acc1566d5a53f110f3b95dd7acde1f10e7c1fc6
Aniruddha <aniruddha.murali@gmail.com>
2021-12-23 02:22:52 -0500

    Add details in footer, add spacing/padding for icons

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -11,7 +11,27 @@ class Footer extends Component {
     render() {
         return (
             <footer>
-                <div class="d-flex flex-wrap justify-content-center">
+                <Container>
+                    <Row className="footerRow">
+                        <Col className="col">
+                            <h4>Location</h4>
+                            <small>Westport, CT</small>
+                        </Col>
+                        <Col className="col">
+                            <h4>Phone</h4>
+                            <small>203-451-8797</small>
+                        </Col>
+                        <Col className="col">
+                            <h4>Web</h4>
+                            <small>aniruddhamurali.com</small>
+                        </Col>
+                        <Col className="col">
+                            <h4>Email</h4>
+                            <small>aniruddha.murali@gmail.com</small>
+                        </Col>
+                    </Row>
+                </Container>
+                <div className="d-flex flex-wrap justify-content-center icons">
                     <a href="https://www.instagram.com/aniruddhamurali/" target="_blank"><i class="fab fa-instagram-square fa-3x"></i></a>
                     <a href="https://twitter.com/AniruddhaMurali" target="_blank"><i class="fab fa-twitter-square fa-3x" ></i></a>
                     <a href="https://www.linkedin.com/in/aniruddhamurali/" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a>
